c1a32f973648bfca2bf967357e2c8185205e8e10
溶酶菌 <hayatesa@live.cn>
2018-09-06 10:49:59 +0800

    主页vue+

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,2 +1,15 @@
 /**
  */
+
+
+/**
+ * 返回
+ * @param code
+ */
+function handleAjax(data) {
+    if (data.code == 401) {
+        window.location.href = '/login';
+    } else {
+        layer.alert(data.msg);
+    }
+}
