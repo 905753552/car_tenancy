9722ba0717fc31a562198d6ffbae9809e95203c6
TLXing <839472561@qq.com>
2018-09-07 09:10:56 +0800

    添加car_menu_data.json

--- src/main/resources/static/js/car_tabs.js
+++ src/main/resources/static/js/car_tabs.js
@@ -1,97 +1,176 @@
-var car_day = new Vue({
-    el:"#carInfoList",
-    data:{
+//当前套餐ID
+var currentPackageId = "carDay";
 
-        menu:[
-            {
-                mid:"carWorkday",
-                mname:"工作日套餐"
-            },{
-                mid:"carWeek",
-                mname:"周套餐"
-            },{
-                mid:"carMon",
-                mname:"月套餐"
-            },{
-                mid:"carYear",
-                mname:"年套餐"
-            }
-        ],
-        items: [
-            {
-                carId:15,
-                carImg:"https://fimg.zuchecdn.com/upload/web/modepic/480.jpg",
-                carName:"奥迪",
-                carVer:"A8",
-                carVan:"三厢",
-                carPower:"1.8自动",
-                carSeat:"6",
-                carPrice:"11",
-                carOriginalPrice:"999",
-                carSavePrice:"111",
-                carHot:"777"
-            },{
-                carId:19,
-                carImg:"https://fimg.zuchecdn.com/upload/web/modepic/480.jpg",
-                carName:"奔驰",
-                carVer:"A8",
-                carVan:"三厢",
-                carPower:"1.8自动",
-                carSeat:"6",
-                carPrice:"113",
-                carOriginalPrice:"999",
-                carSavePrice:"111",
-                carHot:"777"
 
-            },{
-                carId:16,
-                carImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536655028&di=f4827a5065d70760bb21b4c1929c12fc&imgtype=jpg&er=1&src=http%3A%2F%2Fimg4.cache.netease.com%2Fphoto%2F0008%2F2014-10-28%2FA9LNB533294V0008.550x.0.jpg",
-                carName:"宝马",
-                carVer:"5系",
-                carVan:"三厢",
-                carPower:"1.8自动",
-                carSeat:"6",
-                carPrice:"110",
-                carOriginalPrice:"1000",
-                carSavePrice:"111",
-                carHot:"77756"
-            }
-        ]
+const filter_carInfoTab = function () {
+    evenMenus:evenMenus()
+}
+
+
+const carInfoTab_methods = {
+    doSort: doSort,
+    doCarOrder: doCarOrder,
+    doSeachByPackage: doSeachByPackage
+}
+
+
+const init_carInfoTab = function () {
+    loadCarInfoTabData()//加载数据
+}
+
+
+const carInfoTab_app = new Vue({
+    el: "#carInfoTab-app",
+    data: {
+        menu: [],
+        items: []
     },
-    methods:{
-        //订单事件
-        doCarOrder:function (data) {
-            console.log("doCarOrder")
-            console.log(data)
-        }
-    },
+    created: init_carInfoTab(),
+    methods: carInfoTab_methods,
     computed: {
         evenMenus: function () {
             return this.menu.filter(function (mm) {
-                return mm.mname.indexOf("日租自驾")<0
+                return mm.mname.indexOf("日租自驾") < 0
             })
         }
     }
-});
+})
+
 
 
 //排序
 function doSort() {
 
-    if ($("#dev_sort_button").text().indexOf("↓") >=0) {
+    if ($("#dev_sort_button").text().indexOf("↓") >= 0) {
+
         $("#dev_sort_button").html('<span class="dev_od_btn_a">↑</span> 租金由低到高')
-        var sortData = car_day.items;
-        function sortprice(a,b){
+        var sortData = carInfoTab_app.items
+
+        function sortprice(a, b) {
             return a.carPrice > b.carPrice
         }
+
         sortData.sort(sortprice)
-        console.log(sortData)
-    } else{
+
+    } else {
         $("#dev_sort_button").html('<span class="dev_od_btn_a">↓</span> 租金由高到低')
-        var sortData = car_day.items;
-        function sortprice(a,b){
+        var sortData = carInfoTab_app.items
+
+        function sortprice(a, b) {
             return a.carPrice < b.carPrice
         }
         sortData.sort(sortprice)
     }
 }
+
+
+function loadCarInfoTabData() {
+
+    $.ajax({
+        url: "/car_menu_data.json",
+        success: function (data) {
+            if (data.carMenu.code == 500) {
+
+                console.log(data.carMenu.carmenu)
+                //console.log(data.cars.carData)
+                carInfoTab_app.menu = data.carMenu.carmenu
+            } else {
+                handleAjax(data.carMenu);
+            }
+        },
+        fail: function () {
+            console.log("fail");
+        }
+    })
+}
+
+// 过滤菜单
+function evenMenus() {
+    return this.menu.filter(function (mm) {
+        return mm.mname.indexOf("日租自驾") < 0
+    })
+}
+
+//下单
+function doCarOrder() {
+
+}
+
+//按类型查车
+/**
+ *
+ * @param pid 套餐id
+ */
+function doSeachByPackage(pid) {
+    currentPackageId = pid
+    //console.log(nowPackageId)
+    doSeachByAllType()
+}
+
+function doSeachByAllType() {
+
+    // var seachCondition = new Map();
+    // seachCondition.set("carPackage",currentPackageId)//套餐类型
+    // seachCondition.set("carModel",$("#xxx").val() )//车型
+    // seachCondition.set("carBrand",$("#xxx").val() )//品牌
+    // seachCondition.set("carPrice",$("#xxx").val() )//价格
+    // seachCondition.set("carLocation",$("#xxx").val() )//位置
+    // seachCondition.set("carDate",$("#xxx").val() )//日期
+
+    $.ajax({
+        url: "/car_menu_data.json",
+        // data:seachCondition ,
+        success: function (data) {
+            if (data.cars.code == 500) {
+                console.log(data.cars.carData)
+                doHandelCarInfo(data.cars.carData)
+            } else {
+                handleAjax(data.cars);
+            }
+
+        }
+    })
+
+
+    console.log(currentPackageId)
+
+}
+
+function doHandelCarInfo(carData) {
+    let days = 3;//获取租车天数
+    carData.forEach(function (cdata, index) {
+
+        cdata.carTotalPrice = cdata.carPrice * days
+        cdata.carTotalPackagePrice = cdata.carPackagePrice * days
+
+        console.log("------" + cdata.carTotalPackagePrice)
+
+    })
+    carInfoTab_app.items = carData;
+}
+
+
+// var carInfoTab_app = new Vue({
+//     el:"#carInfoTab-app",
+//     data:{
+//         menu:[],
+//         items:[]
+//     },
+//     created:function(){
+//         loadCarInfoTabData()
+//     },
+//     methods:{
+//         //订单事件
+//         doCarOrder:function (data) {
+//             console.log("doCarOrder")
+//             console.log(data)
+//         }
+//     },
+//     computed: {
+//     evenMenus: function () {
+//         return this.menu.filter(function (mm) {
+//             return mm.mname.indexOf("日租自驾")<0
+//         })
+//     }
+//     }
+// });
