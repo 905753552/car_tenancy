675ab2d08e0fc64d56184b1ff896f2862a41ca82
溶酶菌 <hayatesa@live.cn>
2018-09-21 12:51:23 +0800

    登录用户API增加权限控制

--- src/main/java/com/dev/main/shiro/controller/TokenController.java
+++ src/main/java/com/dev/main/shiro/controller/TokenController.java
@@ -3,6 +3,7 @@ package com.dev.main.shiro.controller;
 import com.dev.main.common.util.ResultMap;
 import com.dev.main.shiro.util.ShiroUtils;
 import com.dev.main.tenancy.domain.TncCustomer;
+import org.apache.shiro.authz.annotation.RequiresUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class TokenController {
      * 获取登录的用户
      */
     @GetMapping
+    @RequiresUser
     public ResultMap token() {
         TncCustomer token = ShiroUtils.getUserEntity();
         // 密码安全，设置密码和盐值为空串
