82ed316944efe2893f21d65b3f3ef08fa69b94dd
溶酶菌 <hayatesa@live.cn>
2018-09-21 12:50:00 +0800

    增加获取登录用户API

---
+++ src/main/java/com/dev/main/shiro/controller/TokenController.java
@@ -0,0 +1,26 @@
+package com.dev.main.shiro.controller;
+
+import com.dev.main.common.util.ResultMap;
+import com.dev.main.shiro.util.ShiroUtils;
+import com.dev.main.tenancy.domain.TncCustomer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/shiro/token")
+public class TokenController {
+
+    /**
+     * 获取登录的用户
+     */
+    @GetMapping
+    public ResultMap token() {
+        TncCustomer token = ShiroUtils.getUserEntity();
+        // 密码安全，设置密码和盐值为空串
+        token.setPassword("");
+        token.setSalt("");
+        return ResultMap.success().put("token", token);
+    }
+
+}
