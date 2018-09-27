d3d73240c4590bf6251ecf10fc505d44b3620333
LinAccount <37659071+LinAccount@users.noreply.github.com>
2018-09-27 17:30:48 +0800

    下单

---
+++ src/main/java/com/dev/main/tenancy/service/IOrderService.java
@@ -0,0 +1,9 @@
+package com.dev.main.tenancy.service;
+
+/**
+ *  * Description: main
+ *  * Created by sf on 2018/9/25 8:36
+ *  
+ */
+public interface IOrderService {
+}
---
+++ src/main/java/com/dev/main/tenancy/service/impl/OrderService.java
@@ -0,0 +1,9 @@
+package com.dev.main.tenancy.service.impl;
+
+/**
+ *  * Description: main
+ *  * Created by sf on 2018/9/25 8:37
+ *  
+ */
+public class OrderService {
+}
