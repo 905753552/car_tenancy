857eda6e796053d9b18870385fa0cdf5eab1f202
devl <devl@rsmzjp.top>
2018-10-15 15:04:49 +0800

    添加打包相关配置

--- src/main/java/com/dev/main/MainApplication.java
+++ src/main/java/com/dev/main/MainApplication.java
@@ -3,7 +3,9 @@ package com.dev.main;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,7 +14,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement//开启事务管理
 @MapperScan({"com.dev.main.tenancy.dao", "com.dev.main.tenancy.dao"})//与dao层的@Mapper二选一写上即可(0主要作用是扫包)
 @EnableCaching
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
